0e5608d556c781f75b2de58628e33b59c097d705
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-10-24 19:21:39 +0900

    update html syntax

--- ui/src/page/index/server/render.jsx
+++ ui/src/page/index/server/render.jsx
@@ -13,19 +13,18 @@ const render = (preloadedState, metas) => {
       <App />
     </Provider>,
   );
-  return `<!doctype html>
-    <html>
-      <head>
-        <title>${escape(metas.title)}</title>
-      </head>
-      <body>
-        <div id="root">${root}</div>
-        <script>
-          window.__PRELOADED_STATE__ = ${serialize(preloadedState)}
-        </script>
-        <script src="/build/index.bundle.js"></script>
-      </body>
-    </html>
+  return `<!DOCTYPE html>
+  <head>
+    <title>${escape(metas.title)}</title>
+  </head>
+  <body>
+    <div id="root">${root}</div>
+    <script>
+      window.__PRELOADED_STATE__ = ${serialize(preloadedState)}
+    </script>
+    <script src="/build/index.bundle.js"></script>
+  </body>
+</html>
 `;
 };
 

0098c633ea7baa038c128d640d62859a997109f4
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-10-24 19:22:00 +0900

    update skeleton menu

--- ui/dev/index.php
+++ ui/dev/index.php
@@ -5,7 +5,12 @@ init: {
   $list = '';
   foreach (glob("config/*.php") as $file) {
     $path = pathinfo($file);
-    $list .= "<li><a href=\"?page={$path['filename']}\">{$path['filename']}</a></li>\n";
+    $list .= "<li><a href=\"?page={$path['filename']}\">{$path['filename']}</a>";
+    if ($path['filename'] === $page) {
+        $list .= " ✔︎</li>\n";
+    } else {
+        $list .= "</li>\n";
+    }
   }
   $hasSsr = file_exists(__DIR__ . "/build/{$app}_ssr.bundle.js");
 }
@@ -17,18 +22,18 @@ init: {
   <!-- Latest compiled and minified CSS -->
 </head>
   <body>
-  <h1><?= $app ?>.bundle.js - <?= $page ?>.php </h1>
-  <h2>Rendering method</h2>
-  <ui>
-    <li><a href="csr.php?page=<?= $page ?>">Client Side</a></li>
-<?php if ($hasSsr): ?>
-    <li><a href="ssr.php?page=<?= $page ?>">Server Side</a>
-    <li><a href="ssr-dev.php?page=<?= $page ?>">Server Side (run on client)</a></li>
-<?php endif; ?>
   </ui>
-  <h2>Available UI</h2>
+  <h2>1) Choose ui/config</h2>
   <ui>
       <?= $list ?>
   </ui>
+
+  <h2>2) Choose rendering method</h2>
+  <ui>
+      <li><a href="csr.php?page=<?= $page ?>">Client Side</a></li>
+      <?php if ($hasSsr): ?>
+          <li><a href="ssr.php?page=<?= $page ?>">Server Side</a>
+          <li><a href="ssr-dev.php?page=<?= $page ?>">Client Side with SSR Code</a></li>
+      <?php endif; ?>
   </body>
 </html>
